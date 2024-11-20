e189efcecf57b54ac4b37bc3f2c199b2372cb3c1
Vediniz <veruskalima138@gmail.com>
2024-11-19 21:55:57 -0300

    adding a default user
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

---
+++ src/main/java/com/github/vediniz/jwtauth/config/AdminInitializer.java
@@ -0,0 +1,29 @@
+package com.github.vediniz.jwtauth.config;
+
+import com.github.vediniz.jwtauth.entity.User;
+import com.github.vediniz.jwtauth.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AdminInitializer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        if (userRepository.findByUsername("admin") == null) {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin"));
+            admin.setRole("ADMIN");
+            userRepository.save(admin);
+        }
+    }
+}
