c3684e0a0196be2b3c3807dfe833bd368c35874f
Vediniz <veruskalima138@gmail.com>
2024-11-19 21:54:57 -0300

    adding services
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

---
+++ src/main/java/com/github/vediniz/jwtauth/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.github.vediniz.jwtauth.services;
+
+import com.github.vediniz.jwtauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.github.vediniz.jwtauth.entity.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
